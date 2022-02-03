e940bbd58b197b80d32aba2fa555605e8019f1d5
MikeTroianello <mike.troianello@gmail.com>
2022-02-03 14:38:36 -0500

    added form

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,7 @@ router.get("/add-user", function (req, res, next) {
     .then((results) => {
       console.log("User was created", results);
       res.render("user-page", {
-        username: results.username,
-        favoriteIceCream: results.favoriteIceCream,
+        user: results,
       });
     })
     .catch((err) => {
@@ -26,7 +25,6 @@ router.get("/add-user", function (req, res, next) {
 });
 
 router.get("/find-user/:username", function (req, res) {
-  //Carlos
   User.findOne({ username: req.params.username })
     .then((results) => {
       console.log("User was found", results);
@@ -40,9 +38,24 @@ router.get("/find-user/:username", function (req, res) {
     });
 });
 
+router.get("/user/:userId", function (req, res) {
+  User.findById(req.params.userId)
+    .then((results) => {
+      console.log("User was found", results);
+      res.render("user-page", results);
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+});
+
+router.get("/create-user", (req, res) => {
+  res.render("create-user");
+});
+
 //create a new user
 router.post("/create-user", function (req, res, next) {
-  // res.json(req.body);
+  console.log("THE POST BODY", req.body);
   User.create({
     username: req.body.username,
     password: req.body.password,
@@ -59,5 +72,77 @@ router.post("/create-user", function (req, res, next) {
       console.log("Something went wrong", err);
     });
 });
+
+router.get("/all-users", (req, res) => {
+  User.find()
+    .then((results) => {
+      console.log("Users found:", results);
+      res.render("all-users", { users: results });
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+});
+
+// router.get("/seed", (req, res) => {
+//   let users = [
+//     {
+//       username: "Nick",
+//       password: "password1",
+//       favoriteIceCream: "chocolate",
+//     },
+//     {
+//       username: "Manny",
+//       password: "password1",
+//       favoriteIceCream: "vanilla",
+//     },
+//     {
+//       username: "Yaron",
+//       password: "password1",
+//       favoriteIceCream: "strawberry",
+//     },
+//     {
+//       username: "Juan",
+//       password: "password1",
+//       favoriteIceCream: "chocolate",
+//     },
+//     {
+//       username: "Lauren",
+//       password: "password1",
+//       favoriteIceCream: "chocolate",
+//     },
+//     {
+//       username: "Hailey",
+//       password: "password1",
+//       favoriteIceCream: "strawberry",
+//     },
+//     {
+//       username: "Jesse",
+//       password: "password1",
+//     },
+//     {
+//       username: "Wendy",
+//       password: "password1",
+//       favoriteIceCream: "vanilla",
+//     },
+//     {
+//       username: "Ryan",
+//       password: "password1",
+//       favoriteIceCream: "vanilla",
+//     },
+//   ];
+//   User.create(users)
+//     .then((results) => {
+//       console.log("User was created", results);
+//       res.json(results);
+//       // res.render("user-page", {
+//       //   username: results.username,
+//       //   favoriteIceCream: results.favoriteIceCream,
+//       // });
+//     })
+//     .catch((err) => {
+//       console.log("Something went wrong", err);
+//     });
+// });
 
 module.exports = router;
